44fb6813de753dbcb2fea0693ee3aa3d614aa56b
andres-sanabria-10 <andres.sanabria02@uptc.edu.co>
2025-06-13 11:51:16 -0500

    first commit

---
+++ Router/Rutas.py
@@ -0,0 +1,31 @@
+from flask import request, jsonify
+
+def registrar_rutas(app):
+
+    @app.route('/enviar-datos', methods=['POST'])
+    def enviar_datos():
+        try:
+            data = request.json
+            client = app.config['SHEETS_CLIENT']
+            spreadsheet_id = app.config['SPREADSHEET_ID']
+            sheet = client.open_by_key(spreadsheet_id).sheet1
+
+            sheet.append_row([
+                data.get("Maquina"),
+                data.get("Fecha"),
+                data.get("Turno"),
+                data.get("HR_reporte"),
+                data.get("Hr_Inicio"),
+                data.get("Hr_Final"),
+                data.get("Tiempo_Falla"),
+                data.get("Horómetro"),
+                data.get("Tiempo_de_falla"),
+                data.get("Falla"),
+                data.get("Nombre_Mecanico"),
+                data.get("Diagnostico"),
+                data.get("Labor_Realizada")
+            ])
+
+            return jsonify({"message": "Datos enviados correctamente"}), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+import json
+from flask import Flask
+from dotenv import load_dotenv
+import gspread
+from google.oauth2.service_account import Credentials
+from Router.Rutas import registrar_rutas
+
+# Cargar variables de entorno
+load_dotenv()
+
+# Configurar Flask
+app = Flask(__name__)
+
+# Configurar Google Sheets
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+google_creds = os.getenv('GOOGLE_CREDS_JSON')
+info = json.loads(google_creds)
+creds = Credentials.from_service_account_info(info, scopes=SCOPES)
+client = gspread.authorize(creds)
+
+# Guardar cliente y spreadsheet_id en la app
+app.config['SHEETS_CLIENT'] = client
+app.config['SPREADSHEET_ID'] = os.getenv('SPREADSHEET_ID')
+
+# Registrar rutas desde routes.py
+registrar_rutas(app)
+
+# Ejecutar servidor Flask
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))  # Usa el puerto definido en .env o 5000 por defecto
+    app.run(debug=True, port=port)
